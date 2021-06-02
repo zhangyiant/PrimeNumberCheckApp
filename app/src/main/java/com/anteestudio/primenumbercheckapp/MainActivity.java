8bc6fe515eb36b9ba0b6b306587e0443d6fd8e71
Yi Zhang <zhangyiant@hotmail.com>
2021-06-02 21:14:42 +0800

    init commit

---
+++ app/src/main/java/com/anteestudio/primenumbercheckapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anteestudio.primenumbercheckapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
